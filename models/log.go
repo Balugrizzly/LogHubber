ab47c557c0ed3215719844af4bce20a56ecfb3ec
Balu <baluproton+gitlab@protonmail.com>
2022-11-02 19:02:05 +0100

    Change AccessToken to Token And use Float for Timestamp

--- models/log.go
+++ models/log.go
@@ -6,16 +6,16 @@ import (
 
 type Log struct {
 	gorm.Model
-	AccessToken string `json:"AccessToken" gorm:"not null"`
+	Token string `json:"Token" gorm:"not null"`
 
 	// Supported Log Levels are:
 	// DEBUG,INFO,WARNING,ERROR,CRITICAL,FATAL
-	Level     string `json:"Level"`
-	Timestamp uint   `json:"Timestamp"`
-	Log       string `json:"Log"`
-	Project   string `json:"Project"`
-	App       string `json:"App"`
-	Client    string `json:"Client"`
+	Level     string  `json:"Level"`
+	Timestamp float64 `json:"Timestamp"`
+	Log       string  `json:"Log"`
+	Project   string  `json:"Project"`
+	App       string  `json:"App"`
+	Client    string  `json:"Client"`
 }
 
 func (l *Log) IsValid() (bool, error) {
